8004e3da0a21a2a5cef2c449a8e56f29371e19af
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 15:21:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,3 +17,22 @@ def hopper
      }
    programmer_hash[:grace_hopper]
 end
+
+def alan_kay_is_known_for
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+   programmer_hash[:alan_kay][:known_for]
+end
