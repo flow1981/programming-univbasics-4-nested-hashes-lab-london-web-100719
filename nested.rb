c12097d8859876f5594011d632e8e20df7cc6434
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 15:30:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,3 +102,27 @@ def changing_alan
    programmer_hash[:alan_kay][:known_for]="GUI"
    programmer_hash
 end
+
+def adding_dennis
+	programmer_hash = 
+ 		{
+ 		    :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+        },
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+   programmer_hash[:alan_kay][:known_for]="GUI"
+   programmer_hash
+end
