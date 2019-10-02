08f6c54968716622befe961e300f4a41f7817b8c
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 15:24:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
-def dennis_ritchies_language
+def adding_matz
 	programmer_hash = 
  		{
         :grace_hopper => {
