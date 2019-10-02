fd3435ba57d67ef95bea224197f7d8cd14953b6e
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 15:31:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,6 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-   programmer_hash[:alan_kay][:known_for]="GUI"
+   programmer_hash[:dennis_ritchie][:languages] <<"Assembly"
    programmer_hash
 end
